999b7bac700714388101dcdd316f28870a08cddf
ismasampa <ismael.santos@fato-ti.com.br>
2021-10-30 13:35:55 -0300

    Wife is out and Baby weakup

---
+++ aspnet-core/Klir.TechChallenge.Tests/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Klir.TechChallenge.Tests
+{
+    class Data
+    {
+        [Fact]
+        public void List_Products_Repository()
+        {
+            Assert.NotEmpty(products);
+        }
+        
+    }
+}
